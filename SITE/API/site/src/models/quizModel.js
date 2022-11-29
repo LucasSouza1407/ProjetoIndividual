9cadc67d7c2a849b23c9a1861f9dffcf0f129a3c
LucasSouza1407 <lucasmilagres@sptech.school>
2022-11-29 15:04:56 -0300

    API finalizada

--- SITE/API/site/src/models/quizModel.js
+++ SITE/API/site/src/models/quizModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function listar(nome) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
     var instrucao = `
-        SELECT * FROM pontuacao WHERE nome = ${nome};
+    SELECT nome,pontos FROM pontuacao order by pontos desc;
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
